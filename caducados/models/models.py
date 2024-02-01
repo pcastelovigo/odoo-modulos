f6ba3dda027c3f9a2b2955d8325c43cdbe7b7772
Pablo Manuel Castelo Vigo <pablocv@gmx.es>
2024-02-01 18:39:30 +0100

    en la 16, cambia

--- caducados/models/models.py
+++ caducados/models/models.py
@@ -6,5 +6,5 @@ import datetime
 class StockMoveLine(models.Model):
 	_inherit = "stock.move.line"
 
-	lot_id = fields.Many2one('stock.production.lot', 'Lot/Serial Number', domain="[('product_id', '=', product_id), ('company_id', '=', company_id), ('expiration_date', '>', datetime.datetime.now())]", check_company=True)
+	lot_id = fields.Many2one('stock.lot', 'Lot/Serial Number', domain="[('product_id', '=', product_id), ('company_id', '=', company_id), ('expiration_date', '>', datetime.datetime.now())]", check_company=True)
 
